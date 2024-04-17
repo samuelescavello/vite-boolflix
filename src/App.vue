f96fd19428d60f2bdb60e463cd6352cfa731d59d
samuelescavello <scavellosamuele23@gmail.com>
2024-04-17 12:02:17 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
-  <h1 class="text-center">Ciao</h1>
-  <i class="fa fa-solid fa-home"></i>
+  
 </template>
 
 <script>
+import axios from 'axios';
+import {store} from './store.js';
   export default {
-    name: 'App'
+    name: 'App',
+    data() {
+      return {
+        store
+      }
+    },
+    methods: {
+      getMovies() {
+        axios.get(this.store.apiUrl+this.store.endPoint.movie, this.store.options).then((res) => {
+          console.log(res.data.results);
+        })
+      },
+      getTv() {
+       axios.get(this.store.apiUrl+this.store.endPoint.serieTv, this.store.options).then((res) => {
+         console.log(res.data.results);
+       })
+     },
+     
+    },
+  
+    created(){
+      this.getMovies();
+      this.getTv();
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+
 
 </style>
---
+++ src/store.js
@@ -0,0 +1,15 @@
+import {reactive} from "vue";
+
+export const store = reactive({
+    apiUrl: 'https://api.themoviedb.org/3/',
+    endPoint:{
+        movie:'search/movie',
+        serieTv: 'search/tv'
+    },
+    options:{
+        params:{
+            api_key: 'edbb48568da179f473b1480294ce4f47',
+            query:'a'
+        }
+    },
+});
