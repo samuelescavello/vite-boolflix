cfd4d332660f562d2881f6a625a639e0ce40c547
samuelescavello <scavellosamuele23@gmail.com>
2024-04-17 19:48:11 +0200

    card flip fix

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,26 +1,72 @@
 <template>
-   
-    <div class="card my-2">
-        <img :src="img" class="card-img-top" :alt="title">
-        <div class="card-body">
-            <h5 class="card-title text-uppercase text-center">{{title}}</h5>
-            <p class="card-text text-center">
-                <span>{{ secondTitle }}</span>
-                <span>{{ lenguages }}</span>
-                <span>{{ vote }}</span>
-            </p>
-        </div>
 
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img :src="img" :alt="title" >
+            </div>
+            <div class="flip-card-back  ">
+                <div class="pt-5">
+                    <p>
+                        <h6>original tile: <br> {{ title }}</h6>
+                        <h6>second title: <br> {{ secondTitle }}</h6>
+                        <h6>original language: <br>{{ language }}</h6>
+                        <h6>vote: <br>{{ vote }}</h6>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'CardComponent',
-        props:['title','secondTitle','lenguages','vote','img'],
-    }
+export default {
+    name: 'CardComponent',
+    props: ['title', 'secondTitle', 'language', 'vote', 'img'],
+}
 </script>
 
 <style lang="scss" scoped>
+.flip-card {
+  background-color: transparent;
+  width: 300px;
+  height: 470px;
+  perspective: 1000px; 
+  margin-bottom: 30px;
+}
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; 
+  backface-visibility: hidden;
+}
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+.flip-card-back {
+  background-color: black;
+  color: white;
+  transform: rotateY(180deg);
+}
+img{
+    width: 100%;
+    height:100%;
+}
+h6{
+    padding-bottom: 20px;
+}
 
 </style>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,9 +1,19 @@
 <template>
     
     <div class="container">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-5" >
+        <h2>film</h2>
+        <div class="row " >
             <div class="col" v-for="movie in store.movies">
-                <CardComponent :img="store.imageUrl + movie.poster_path" :id="movie.id" :title="movie.original_title" :secondTitle="movie.title" :lenguages="movie.original_lenguages" :vote="movie.vote_average"/>
+                <CardComponent :img="store.imageUrl + movie.poster_path"  :title="movie.original_title" :secondTitle="movie.title" :language="movie.original_language" :vote="movie.vote_average"/>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <h2>serietv</h2>
+        <div class="row" >
+            <div class="col" v-for="serieTV in store.series">
+                <CardComponent :img="store.imageUrl + serieTV.poster_path" :id="serieTV.id" :title="serieTV.original_name" :secondTitle="serieTV.name" :lenguages="serieTV.original_lenguages" :vote="serieTV.vote_average"/>
             </div>
         </div>
     </div>
