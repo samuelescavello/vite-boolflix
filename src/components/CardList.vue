d5115c14adcbffa4ea5dc376702d251ca52690b7
samuelescavello <scavellosamuele23@gmail.com>
2024-04-18 16:59:48 +0200

    fix

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,7 +1,9 @@
 <template>
     
     <div class="container">
-        <h2>film</h2>
+        <div v-if="store.movies.length > 0">
+            <h2  class="text-uppercase">film</h2>
+        </div>
         <div class="row " >
             <div class="col" v-for="movie in store.movies">
                 <CardComponent :img="store.imageUrl + movie.poster_path"  :title="movie.original_title" :secondTitle="movie.title" :language="movie.original_language" :vote="movie.vote_average" :overview="movie.overview"/>
@@ -10,7 +12,9 @@
     </div>
 
     <div class="container">
-        <h2>serietv</h2>
+        <div v-if="store.series.length > 0">
+            <h2 class="text-uppercase">serietv</h2>
+        </div>
         <div class="row" >
             <div class="col" v-for="serieTV in store.series">
                 <CardComponent :img="store.imageUrl + serieTV.poster_path" :id="serieTV.id" :title="serieTV.original_name" :secondTitle="serieTV.name" :language="serieTV.original_language" :vote="serieTV.vote_average" :overview="serieTV.overview"/>
@@ -37,5 +41,7 @@ import CardComponent from './CardComponent.vue';
 </script>
 
 <style lang="scss" scoped>
-
+    h2{
+        color: red;
+    }
 </style>
